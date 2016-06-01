30cf62e1405c0b6cadf51b32fbd70b1c8a161c32
Stefen <Stefen.Phelps@accuform.local>
2016-06-01 14:19:42 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# HubSpot COS Off-Canvas Menu
+An easy way to implement an off-canvas menu using the HubSpot COS.
+## Getting Started
+	* Add the styles.css and main.js to your page (or template).
+	* Add an Advanced Menu module to the template and give it a class "off-canvas-menu".
+	* Add a link with the class "menu-toggle" on your page e.g. <a href="#" class="menu-toggle">Menu</a>
---
+++ js/OffCanvasMenu.js
@@ -0,0 +1,19 @@
+    /////////////////////// Off-Canvas Menu
+    
+    // Toggle Menu
+    $('.menu-toggle').on('click', function(e){
+        e.stopPropagation();
+        $('body').toggleClass('show-menu');
+        return false;
+    });
+    
+    // Hide menu if click is outside of menu
+    $(document).on('click', function (e) {
+        $('body').removeClass('show-menu');
+    });
+    
+    // Toggle second level navigation
+    $('.off-canvas-menu .hs-menu-wrapper > ul > li.hs-item-has-children > a').click(function() {
+        $(this).parent().toggleClass('show-menu');
+        return false;
+    });
